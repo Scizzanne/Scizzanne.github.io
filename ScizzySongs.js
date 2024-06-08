c9d3273919e956e2222af4dd9c4d24cf58baf614
Scizzy <149926085+Scizzanne@users.noreply.github.com>
2024-06-08 12:15:20 -0700

    ScizzySongs.js

--- ScizzySongs.js
+++ ScizzySongs.js
@@ -1,9 +1,15 @@
+/* 
+    Uhhh yeah! Hi!!! this is the uhhh thingy that make da
+    somgs work n stuff!!! Nice to see you here!!! :3
+*/
+
 // Set Songs 
 var scizz = document.getElementById("ScizzTheme"); 
 var heart = document.getElementById("Hearthome"); 
 var black = document.getElementById("BlackTower"); 
 var great = document.getElementById("GreatCanyon");
-
+var overt = document.getElementById("Overture")
+var WiFi = document.getElementById("WiFiConnection")
 
 // Random Integer 
 function getRandomInt(max) {
@@ -12,15 +18,21 @@ function getRandomInt(max) {
 song = getRandomInt(100);
 
 // Song Selector 
-if (song < 25) {
+if (song < 16) {
     var x = black;
 
-} else if (song < 50) {
+} else if (song < 32) {
     var x = heart;
 
-} else if (song < 75) {
+} else if (song < 48) {
     var x = great;
 
+} else if (song < 64) {
+    var x = overt;
+
+} else if (song < 80) {
+    var x = WiFi;
+    
 } else {
     var x = scizz;
 }
@@ -32,3 +44,4 @@ function playAudio() {
 function pauseAudio() { 
     x.pause(); 
 } 
+
