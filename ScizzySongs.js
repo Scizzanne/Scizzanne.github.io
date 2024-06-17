e728d6cc2bf4bf2c86d860a8e48c970ef303bdd9
Scizzy <149926085+Scizzanne@users.noreply.github.com>
2024-06-17 10:16:36 -0700

    ScizzySongs.js

--- ScizzySongs.js
+++ ScizzySongs.js
@@ -20,19 +20,14 @@ song = getRandomInt(100);
 // Song Selector 
 if (song < 16) {
     var x = black;
-
 } else if (song < 32) {
     var x = heart;
-
 } else if (song < 48) {
     var x = great;
-
 } else if (song < 64) {
     var x = overt;
-
 } else if (song < 80) {
     var x = WiFi;
-    
 } else {
     var x = scizz;
 }
@@ -45,3 +40,20 @@ function pauseAudio() {
     x.pause(); 
 } 
 
+// TODO: Produce Message - SHOULD BE INVISIBLE FIRST
+
+// Shows hidden when content is on screen 
+// (set class to hidden to activate)
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else {
+                entry.target.classList.remove('show');
+            }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
