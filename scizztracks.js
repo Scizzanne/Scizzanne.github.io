3e2aea8fe33ff785515842ce6e68f9d7ba374bcd
Renzo <scizzanne@gmail.com>
2026-04-23 23:15:11 -0700

    Added Scizztracks!!!

---
+++ scizztracks.js
@@ -0,0 +1,9 @@
+function copyCurrentUrl() {
+  const url = window.location.href;
+  
+  navigator.clipboard.writeText(url).then(() => {
+    alert("URL copied to clipboard!");
+  }).catch(err => {
+    console.error("Failed to copy: ", err);
+  });
+}
