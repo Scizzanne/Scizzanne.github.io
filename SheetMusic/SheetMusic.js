115192a6ca536608689daf5e25c13182fe9dadfc
Renzo <scizzanne@gmail.com>
2026-09-12 21:02:07 -0700

    sheet music loadable

--- SheetMusic/SheetMusic.js
+++ SheetMusic/SheetMusic.js
@@ -1,3 +1,26 @@
 /**
  * This file is to load all sheet music I have made!
  */
+
+var sheetMusicIndex = 0;
+var sheetMusicData = null; // array of objects containing the sheet music data
+var initialized = false;
+
+// load the data and then load the sheet music
+init().then(() => {
+    loadData();
+});
+
+// wait until the data is loaded before allowing access to it
+async function init() {
+    if (initialized) return;
+
+    const res = await fetch("./SheetMusicFiles.json");
+    sheetMusicData = await res.json(); 
+
+    initialized = true;
+}
+
+function loadData() {
+    console.log(sheetMusicData);
+}
